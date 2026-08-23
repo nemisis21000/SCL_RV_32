1543ebd281ed162ab323814bbc14a60ca69a1ca5
nemisis21000 <kushagra2006gupta@gmail.com>
2026-08-23 19:50:06 +0530

    Removed vivado useless text

--- src/Adder.sv
+++ src/Adder.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 19.05.2026 22:51:01
-// Design Name: 
-// Module Name: Adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Adder(
 input logic [31:0] PCE,
